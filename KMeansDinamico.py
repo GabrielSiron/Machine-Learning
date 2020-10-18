8ddfbae9c8830d99d9cda80882e43f2ad22db782
Gabriel Menezes <=>
2020-10-17 23:22:13 -0300

    nao esta funcionando. Vou tentar resolver

--- KMeansDinamico.py
+++ KMeansDinamico.py
@@ -1,9 +1,8 @@
-import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from sklearn.cluster import KMeans
 
+'''
 dataset = np.array([[178, 77], 
                     [154, 64],
                     [160, 60],
@@ -25,31 +24,92 @@ dataset = np.array([[178, 77],
                     [150, 50],
                     [176, 79]])
 
-kmeans = KMeans(n_clusters = 3, 
-        init='random',
-        n_init=1,
-        max_iter=1)
+'''
 
-figure = plt.figure()
-ax = figure.add_subplot(1, 1, 1)
-qtd = 3
+
+clusters = 3
+features = 3
+dataset = np.random.rand(250, features)*100
+if features == 3:
+    figure = plt.figure()
+    ax = figure.add_subplot(111, projection='3d')
+    #ax.scatter(args[0], args[1], args[2], s = 70, c = predict_y + [5, 5, 5])
+if features == 2:
+    figure = plt.figure()
+    ax = figure.add_subplot(1, 1, 1)
+    #
+
 predict_y = []
-list_center = [0]*qtd
+list_center = [0]*clusters
 
-aux1 = 0
-aux2 = 0
-aux3 = 0
+center = np.random.rand(clusters, features)*100
+#center = [[100, 57, 80], [150, 60, 100], [170, 80, 100]]
 
-for j in range(10):
-    if j > 0:
-        print(kmeans.cluster_centers_)
-    predict_y.append(kmeans.fit_predict(dataset))
-    print(kmeans.cluster_centers_)
+for j in range(3):
+    
+    dist = [[], [], []]
+    predict = []
+    
+    for i in range(clusters):
+        soma = 0
+        for k in range(len(dataset)):
+            for y in range(features):
+                soma += (center[i][y] - dataset[k][y])**2
+            dist[i].append(pow(soma, 0.5))
+    
+    for i in range(len(dist[0])):
+        comp = []
+        for j in range(clusters):
+            comp.append(dist[j][i])
+        
+        predict.append(comp.index(min(comp))) 
+            
+    print(len(dist))
 
-copy_x = list(dataset[:,0])
-copy_y = list(dataset[:,1])
-for i in range(10):
-    ax.clear()
-    ax.scatter(copy_x, copy_y, s = 70, c = predict_y[i])
+    args   = [[], [], []]
+    for a in range(features):
+        for r in range(len(dataset)):
+            args[a].append(dataset[r][a])
+        for t in range(len(center)):
+            args[a].append(center[t][a])
+
+    if features == 2:
+        ax.scatter(args[0], args[1], s = 70, c = predict + [5, 5, 5])
+    elif features == 3:
+        ax.scatter(args[0], args[1], args[2], s = 70, c = predict + [5, 5, 5])
+
     plt.draw()
-    plt.pause(0.001)
+    plt.pause(0.2)
+    ax.clear()
+    medias = []
+    for q in range(clusters):
+        medias.append([0]*features)
+
+    for b in range(len(predict_y)):
+        for u in range(features):
+            medias[u].append(dataset[b][u])
+    
+    #print(medias)
+    center = [[0]*features]*clusters
+    #print(center)
+    
+    a = 0
+
+    for k in range(len(dataset)):
+        for j in range(clusters):
+            if predict[k] == j:
+                for i in range(features):
+                    center[predict[k]][i] += dataset[k][i]
+                a += 1
+                
+    print(predict)
+    
+    '''
+    medias[0] = sum(mediasx[0])/(len(mediasx[0]) + 0.001)
+    mediasx[1] = sum(mediasx[1])/(len(mediasx[1]) + 0.001)
+    mediasx[2] = sum(mediasx[2])/(len(mediasx[2]) + 0.001)
+        
+    mediasy[0] = sum(mediasy[0])/(len(mediasy[0]) + 0.001)
+    mediasy[1] = sum(mediasy[1])/(len(mediasy[1]) + 0.001)
+    mediasy[2] = sum(mediasy[2])/(len(mediasy[2]) + 0.001)
+    '''
